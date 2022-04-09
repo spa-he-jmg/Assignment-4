8639f30dfb06249f18f331afe2f7b3741f026224
Juan Miguel Gonzalez <shut.haven@gmail.com>
2022-04-09 17:15:34 -0500

    base for wordle

---
+++ app.js
@@ -0,0 +1,17 @@
+import { Game } from './Game.js';
+
+async function initVocab() {
+    const response = await fetch('https://murmuring-atoll-65515.herokuapp.com/', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    const result = await response.json();
+    return result;
+}
+
+initVocab().then((vocab) => {
+    console.log(vocab);
+});
